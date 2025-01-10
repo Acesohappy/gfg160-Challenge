bc606b2bb8568ef66ba1735e86b20bc49473a416
Anubama <135399669+Acesohappy@users.noreply.github.com>
2025-01-10 20:48:45 +0530

    Create Day34 - Allocate Minimum Pages.java

---
+++ Day34 - Allocate Minimum Pages.java
@@ -0,0 +1,61 @@
+// Java program to find the minimum page limit by iterating
+// over all possible page limits
+
+import java.util.Arrays;
+
+class GfG {
+    
+    // Function to check if books can be allocated to
+    // all k students without exceeding 'pageLimit'
+    static boolean check(int[] arr, int k, int pageLimit) {
+        
+        // Starting from the first student
+        int cnt = 1;
+        int pageSum = 0;
+        for(int i = 0; i < arr.length; i++) {
+            
+            // If adding the current book exceeds the page 
+            // limit, assign the book to the next student
+            if(pageSum + arr[i] > pageLimit) {
+                cnt++;
+                pageSum = arr[i];
+            }
+            else {
+                pageSum += arr[i];
+            }
+        }
+        
+          // If books can assigned to less than k students then
+        // it can be assigned to exactly k students as well
+        return (cnt <= k);
+    }
+
+    static int findPages(int[] arr, int k) {
+        
+        // If number of students are more than total books
+        // then allocation is not possible
+        if(k > arr.length)
+            return -1;
+        
+        // Minimum and maximum possible page limits
+        int minPageLimit = Arrays.stream(arr).max().getAsInt();
+        int maxPageLimit = Arrays.stream(arr).sum();
+
+        // Iterating over all possible page limits
+        for(int i = minPageLimit; i <= maxPageLimit; i++) {
+            
+            // Return the first page limit with we can
+            // allocate books to all k students
+            if(check(arr, k, i))
+                return i;
+        }
+
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {12, 34, 67, 90};
+        int k = 2;
+        System.out.println(findPages(arr, k));
+    }
+}
